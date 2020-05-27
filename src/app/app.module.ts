ce583758a96e364a51115bb7826e0982b67eded6
sulay <sulay.qrioustech@gmail.com>
2020-05-28 00:24:55 +0530

    overlay added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,16 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { ReactiveFormsModule, FormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
 import { OverlayModule } from '@angular/cdk/overlay';
-
+import { HttpClientModule } from '@angular/common/http';
+import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AutoSuggestComponent } from './auto-suggest/auto-suggest.component';
+import { CurrentComponent } from './current/current.component';
+import { DebounceTimeComponent } from './debounce-time/debounce-time.component';
 import { SuggestionItemComponent } from './suggestion-item/suggestion-item.component';
 import { SwitchMapComponent } from './switch-map/switch-map.component';
-import { DebounceTimeComponent } from './debounce-time/debounce-time.component';
-import { CurrentComponent } from './current/current.component';
+import { PortalModule } from '@angular/cdk/portal';
 
 @NgModule({
   declarations: [
@@ -27,7 +27,8 @@ import { CurrentComponent } from './current/current.component';
     OverlayModule,
     ReactiveFormsModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    PortalModule
   ],
   providers: [],
   bootstrap: [AppComponent]
